bf21ffe0c35ec7a7cd6b2c392a509e50dea2ef5f
cat12079801 <cat12079801@gmail.com>
2016-03-25 08:45:33 +0900

    Problem controller/viewを一応見れるようにした

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -2,34 +2,52 @@ class ProblemsController < ApplicationController
   before_action :set_problem, only: [:show, :edit, :update, :destroy]
 
   def index
+    if current_user.administor
+      @problems = Problem.all
+    else
+      @problems = Problem.where(opened: true)
+    end
   end
 
   def show
   end
 
   def new
+    @problem = Problem.new
   end
 
   def edit
   end
 
   def create
+    @problem = Problem.new(problem_params)
+    if @problem.save
+      redirect_to problem_path(@problem), notice: 'Problem was successfully created.'
+    else
+      render :new
+    end
   end
 
   def update
+    if @problem.update(problem_params)
+      redirect_to problem_path(@problem), notice: 'Problem was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @problem.destroy
+    redirect_to problems_url, notice: 'Problem was successfully destroyed.'
   end
 
   private
     def set_problem
       @problem = Problem.find(params[:id])
-      redirect_to problems_path unless @problem.opened
+      redirect_to problems_path, notice: '(´・ω・`)' unless current_user.administor or @problem.opened
     end
  
     def problem_params
-      #params[:problem]
-      params.require(:problem).permit(:title, :question, :flag, :point, :genre_id, :opened)
+      params.require(:problem).permit(:name, :question, :point, :opened)
     end
 end
