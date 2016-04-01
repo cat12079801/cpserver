d6632afe4b0d5615fd9a932e4f8e1bcde346edb2
cat12079801 <cat12079801@gmail.com>
2016-04-02 01:50:27 +0900

    problems/index の view を整備

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,4 +1,12 @@
 class Problem < ApplicationRecord
   has_many :submits, dependent: :destroy
   has_many :test_cases, dependent: :destroy
+
+  def solve_count
+    self.submits.where(status: "AC").pluck(:user_id).uniq.length
+  end
+
+  def solved? user
+    self.submits.where(status: "AC", user: user).empty?.!
+  end
 end
